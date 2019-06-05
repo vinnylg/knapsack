0b09a670aa118d66218130f8dc0952e28f5f502d
Vinicius de Lima Gonçalves <vlg17@inf.ufpr.br>
2019-06-05 20:21:50 -0300

    semif

--- knapsack.c
+++ knapsack.c
@@ -13,21 +13,28 @@ double timestamp(void){
 
 int knapsack(int *value, int *weight, int n_obj, int weight_max, size_t **V){
     int w,                                             //peso iterativo 
-        i;                                             //contador de itens
-    size_t *tmp,   //line one, two and tmp
-            max_value = 0;
+        i,                                         //contador de itensa
+	flag,
+	max,
+	flag2,
+	positivo;
+    size_t *tmp;   //line one, two and tmp
 
         for(i=1; i <= n_obj; i++){                    //percorre apartir do primeiro item até o ultimo (i=0==NULL)
-            for(w = 1; w <= weight_max; w++){               //percorre desde o peso 1 até o peso maximo da mochila
-                if( (weight[i] <= w) && ((max_value = value[i]+V[0][w-weight[i]]) > V[0][w])){    
+            for(w = 1; w <= weight_max; w++){         //percorre desde o peso 1 até o peso maximo da mochila 
+		flag= weight[i]>=w;
+ 		positivo=((w-weight[i])>=0);
+		max=( value[i]+V[0][positivo*(w-weight[i])] ) * positivo;	
+		flag2= max > V[0][w];
+V[1][w]= (flag*flag2*max) + (!flag2*V[0][w]);
+//               if( (weight[i] <= w) && ((max_value = value[i]+V[0][w-weight[i]]) > V[0][w])){    
                     //se o item i caber no peso w E o valor do item i + 
                     //o valor da linha de cima no peso que sobra da mochila com o item i
                     //for maior que o valor do item de cima com o peso w
-                    V[1][w]= max_value;   //coloca essa soma
-                }else{
-                    V[1][w] = V[0][w];                      //senão coloca o valor de cima 
+   //                 V[1][w]= max_value;   //coloca essa soma
+     //           }else{
+       //             V[1][w] = V[0][w];                      //senão coloca o valor de cima 
                 }
-            }
             tmp=V[0];                //tmp recebe linha zero
             V[0]=V[1];                //linha 0 recebe linha 1
             V[1]=tmp;                //linha 1 recebe tmp
