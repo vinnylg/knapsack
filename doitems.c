d08822e10a44cabe180261981f628a12c0c8197d
Vinicius de Lima Gonçalves <vlg17@inf.ufpr.br>
2019-06-09 00:29:02 -0300

    scripts >

--- doitems.c
+++ doitems.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-long int n_obj_exp[11]={1000,2000,4000,8000,16000,32000,64000,128000,256000,512000,1024000};
+long int n_obj_exp[8]={1000,2000,4000,8000,16000,32000,64000,128000};
 long int weight_max_exp[6]={10000,20000,40000,80000,160000,320000};
 long int n_obj_lin[19]={1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000};
-long int weight_max_lin[19]={10000,20000,30000,40000,50000,60000,70000,80000,90000,100000,200000,300000,400000};
+long int weight_max_lin[13]={10000,20000,30000,40000,50000,60000,70000,80000,90000,100000,200000,300000,400000};
 
 int main(){
     int count_files = 0;
     char cmd[100];
     long int weight_max,n_obj;
     for( weight_max=0; weight_max < 6; weight_max++)
-        for (n_obj=0; n_obj < 11; n_obj++){
+        for (n_obj=0; n_obj < 8; n_obj++){
             count_files++;
             sprintf(cmd,"./createItem items/mexp%ld-%ld %ld %ld",weight_max_exp[weight_max],n_obj_exp[n_obj],n_obj_exp[n_obj],weight_max_exp[weight_max]);
             printf("%s\n",cmd);
--- test.sh
+++ test.sh
@@ -7,20 +7,30 @@ DATE="$(date +"%d%k%M%S")"
 OUT=results.$DATE
 echo "INFO-PC" >> $OUT
 lscpu | grep CPU* >> $OUT
+lscpu | grep Thread >> $OUT
 lscpu | grep cache >> $OUT
+L1=$(lscpu | grep  L1)
+L1=${L1#*: }
+L1=${L1%K}
 L2=$(lscpu | grep  L2)
-L2=${L2#L*: }
+L2=${L2#*: }
 L2=${L2%K}
 L3=$(lscpu | grep  L3)
-L3=${L3#L*: }
+L3=${L3#*: }
 L3=${L3%[[:alpha:]]}
 
-if [ "$1" == "L2" ] 
+if [ "$1" == "L1" ] 
 then
+	CACHE=$L1
+elif [ "$1" == "L2" ]
+then 
 	CACHE=$L2
-else 
+else
 	CACHE=$L3
 fi
+
+echo $CACHE 
+echo $DIR
 
 free -m >> $OUT
 
@@ -33,11 +43,11 @@ do
 	echo "$SEQ $ITEM"
 	time $SEQ $DIR$ITEM >> $OUT
 	echo ""
-	for TH in {1..8}
+	for TH in {1..16}
 	do
-    	  echo "$PAR $ITEM with $TH threads and $CACHE of cache"
+    	  echo $PAR $DIR$ITEM $TH $CACHE
     	  time $PAR $DIR$ITEM $TH $CACHE >> $OUT
-		echo ""
+	  echo ""
 	done
 done
 echo "sucess all"
